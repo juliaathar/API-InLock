989119413de29705cdccf8bb63619074f4551f3f
juliaathar <juathar@gmail.com>
2023-09-07 15:59:58 -0300

    configurando o swagger

---
+++ senai.inlock.webApi/Controllers/EstudioController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using senai.inlock.webApi_.Interfaces;
+using senai.inlock.webApi_.Repositories;
+
+namespace senai.inlock.webApi_.Controllers
+{
+    [Route("api/[controller]")]
+
+    [ApiController]
+
+    [Produces("application/json")]
+    public class EstudioController : ControllerBase
+    {
+        private IEstudioRepository _estudioRepository { get; set; }
+
+        public EstudioController()
+        {
+            _estudioRepository = new EstudioRepository();
+        }
+
+    }
+}
--- senai.inlock.webApi/Program.cs
+++ senai.inlock.webApi/Program.cs
@@ -1,6 +1,44 @@
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "API InLock SENAI",
+        Description = "API para gerenciamento de jogos",
+        Contact = new OpenApiContact
+        {
+            Name = "Júlia",
+            Url = new Uri("https://www.linkedin.com/in/juliaathar/")
+        }
+    });
+
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+});
+
+app.MapControllers();
+
+app.UseHttpsRedirection();
 
 app.Run();
